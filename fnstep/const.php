8f6387f6cdca07f19e5c9e3939b40a59b8dfd23b
Valentin Knabel <valentinknabel@Valentins-MacBook-Air.local>
2013-02-26 16:10:03 +0100

    Initial Commit

---
+++ fnstep/const.php
@@ -0,0 +1,20 @@
+<?PHP
+//
+//!FNStep
+//!const.php
+//
+//!Created by Valentin Knabel on 25.02.13
+//!Copyright © 2013 Valentin Knabel. All rights reserved.
+//
+
+namespace FNFoundation;
+
+define('STRING', gettype(''));
+define('CARRAY', gettype(array()));
+define('INTEGER', gettype(0));
+define('FLOAT', gettype(0.0));
+define('RESOURCE', 'resource');
+define('OBJECT', 'object');
+
+?>
+						
---
+++ fnstep/fnstep.php
@@ -0,0 +1,22 @@
+<?PHP
+//
+//!FNStep
+//!fnstep.php
+//
+//!Created by Valentin Knabel on 23.02.13
+//!Copyright (c) 2013 Valentin Knabel. All rights reserved.
+//
+
+require('const.php');
+require('static_functions.php');
+
+require('FNObject.php');
+require('FNContainer.php');
+require('FNString.php');
+require('FNNumber.php');
+require('FNSet.php');
+require('FNArray.php');
+require('FNDictionary.php');
+	
+?>
+						
